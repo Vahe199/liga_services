2b2fea48c5bf496e8cfe1f3dac53608b32653a53
sarkisVorskanyan <vorskanyan.sarkis@mail.ru>
2021-12-27 18:22:32 +0400

    create addNewTask.js

---
+++ src/components/UI/common/customInputAddFile/CustomInputAddFile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {DownloadSvg} from "../../../../assets/svg/DownloadSvg";
+import Box from "@mui/material/Box";
+
+const CustomInputAddFile = ({value, handleChange, name, svg, label}) => {
+    return (
+        <Box>
+            <input
+                color="primary"
+                type="file"
+                name={name}
+                value={value}
+                onChange={handleChange}
+                id="icon-button-file"
+                accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                style={{ display: 'none' }}
+            />
+            <label style={{display: 'flex', marginTop: '10px', justifyContent: 'flex-start', alignItems: 'center'}} htmlFor="icon-button-file">
+                {svg}
+                <p style={{fontSize: '15px', paddingLeft: '20px', color: '#000'}}>{label}</p>
+            </label>
+        </Box>
+    )
+}
+
+export default CustomInputAddFile;
---
+++ src/components/UI/common/customSelect/CustomSelect.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {FormControl, Select} from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import {FormHelperText} from "@material-ui/core";
+import Box from "@mui/material/Box";
+import {useStyles} from "../../../../globalStyles/ModalStyles";
+
+const CustomSelect = ({label, handleChange, value, name, touched, error, mt}) => {
+    const classes = useStyles();
+    return (
+        <Box className={classes.boxInput}>
+            <p style={{marginTop: mt}} className={classes.inputText}>{label}</p>
+            <FormControl style={{height: '10px', marginBottom: '10px'}} fullWidth>
+                <Select
+                    onChange={handleChange}
+                    value={value}
+                    name={name}
+                    error={touched && Boolean(error)}
+                >
+                    <MenuItem value={'Ten'}>Ten</MenuItem>
+                    <MenuItem value={'Twenty'}>Twenty</MenuItem>
+                    <MenuItem value={'Thirty'}>Thirty</MenuItem>
+                </Select>
+                {touched && error && <FormHelperText style={{color: '#F44336', paddingLeft: '15px'}}>{error}</FormHelperText>}
+            </FormControl>
+        </Box>
+    )
+}
+
+export default CustomSelect;
