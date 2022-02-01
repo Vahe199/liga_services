3ac7c31a04d9c83833041848bcc9b9bda91a7813
Your Name <you@example.com>
2022-02-01 17:58:10 +0400

    fix bug in send file

--- src/utils/validation/AddNewOrderValidation.js
+++ src/utils/validation/AddNewOrderValidation.js
@@ -1,43 +1,44 @@
-import {boolean, mixed, object, string} from "yup";
+import {array, boolean, mixed, object, string} from "yup";
 import * as yup from "yup";
 
 export const AddNewOrderValidation = () => object().shape({
 
-    // category_name: string().required('Обязательное поле'),
-    // title: string().required('Обязательное поле').min(3, 'Слишком короткый')
-    //     .max(250, 'Слишком длинный'),
-    // subcategory_name: string().required('Обязательное поле'),
-    // task_description: string().required('Обязательное поле')
-    //     .min(4, 'Слишком короткый')
-    //     .max(250, 'Слишком длинный'),
-    // region: string().when('task_location', {
-    //     is: (task_location) => task_location === 'client',
-    //     then: string().required('Обязательное поле')
-    //         .min(3, 'Слишком короткый')
-    //         .max(250, 'Слишком длинный'),
-    //     otherwise: string(),
-    // }),
-    // address: string().when('task_location', {
-    //     is: (task_location) => task_location === 'client',
-    //     then: string().required('Обязательное поле')
-    //         .min(3, 'Слишком короткый')
-    //         .max(250, 'Слишком длинный'),
-    //     otherwise: string(),
-    // }),
-    // task_starttime: string()
-    //     .required('Обязательное поле'),
-    // task_finishtime: string()
-    //     .required('Обязательное поле'),
-    // price_from: string()
-    //     .required('Обязательное поле')
-    //     .matches(/^[0-9]+$/, "Толко число")
-    //     .min(3, 'Номер телефона должен быть 11 символов')
-    //     .max(18, 'Номер телефона должен быть 11 символов'),
-    // price_to: string()
-    //     .required('Обязательное поле')
-    //     .matches(/^[0-9]+$/, "Толко число")
-    //     .min(3, 'Номер телефона должен быть 11 символов')
-    //     .max(18, 'Номер телефона должен быть 11 символов'),
-   task_img: mixed()
-        .required("Обязательное поле")
+    category_name: string().required('Обязательное поле'),
+    title: string().required('Обязательное поле').min(3, 'Слишком короткый')
+        .max(250, 'Слишком длинный'),
+    subcategory_name: string().required('Обязательное поле'),
+    task_description: string().required('Обязательное поле')
+        .min(4, 'Слишком короткый')
+        .max(250, 'Слишком длинный'),
+    region: string().when('task_location', {
+        is: (task_location) => task_location === 'client',
+        then: string().required('Обязательное поле')
+            .min(3, 'Слишком короткый')
+            .max(250, 'Слишком длинный'),
+        otherwise: string(),
+    }),
+    address: string().when('task_location', {
+        is: (task_location) => task_location === 'client',
+        then: string().required('Обязательное поле')
+            .min(3, 'Слишком короткый')
+            .max(250, 'Слишком длинный'),
+        otherwise: string(),
+    }),
+    task_starttime: string()
+        .required('Обязательное поле'),
+    task_finishtime: string()
+        .required('Обязательное поле'),
+    price_from: string()
+        .required('Обязательное поле')
+        .matches(/^[0-9]+$/, "Толко число")
+        .min(3, 'Номер телефона должен быть 11 символов')
+        .max(18, 'Номер телефона должен быть 11 символов'),
+    price_to: string()
+        .required('Обязательное поле')
+        .matches(/^[0-9]+$/, "Толко число")
+        .min(3, 'Номер телефона должен быть 11 символов')
+        .max(18, 'Номер телефона должен быть 11 символов'),
+    task_img: array().of(mixed().nullable(true).required('Обязательное поле')).min(1, 'В поле должно быть хотя бы 1 элемента.').nullable()
 });
+
+
